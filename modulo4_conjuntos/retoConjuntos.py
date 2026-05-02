7d0892c241d5371b7ebfd1a4163c719f6b8f2d12
cxcristian <cristiandavid3d@gmail.com>
2026-05-01 23:49:52 -0500

    Eliminar archivos .gitkeep innecesarios y actualizar retoConjuntos.py

---
+++ modulo4_conjuntos/retoConjuntos.py
@@ -0,0 +1,4 @@
+# Definir tienda_centro, tienda_norte y tienda_sur
+tienda_centro = {"arepas", "leche", "huevos", "queso"}
+tienda_norte = {"leche", "huevos", "arroz", "pollo"}
+tienda_sur = {"arepas", "arroz", "frijole", "carne"}
